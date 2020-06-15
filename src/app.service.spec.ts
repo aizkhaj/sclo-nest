1100e7812cc5d1d5aa996353b2c96c0c589263d5
Aizaz Khaja <aizazkj@gmail.com>
2020-06-15 07:49:53 -0400

    cleaned up README and attempted testing on app.service, didnt get too far.

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -1,8 +1,15 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppService } from "./app.service";
+import { getRepository } from "typeorm";
+import { Auth } from "./auth/auth.entity";
+
+const mockAuthRepository = () => ({
+
+});
 
 describe("AppService", () => {
   let service: AppService;
+  let authRepository = getRepository(Auth);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
--- src/auth/auth.entity.ts
+++ src/auth/auth.entity.ts
@@ -10,6 +10,7 @@ import { IsString } from "class-validator";
 @Entity()
 @Unique(["login"])
 export class Auth {
+  // normally, a UUID is used and there'd be an extension enabled on Postgres so that it works. For demo purposes, I'm using a number for id.
   @PrimaryGeneratedColumn()
   id: number;
 
